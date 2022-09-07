a7074b4f4fca37f5a7f0ec1d5f4d15279da0edac
Mel Thomas <meljaynethomas@gmail.com>
2022-09-07 15:05:02 +0100

    test file deleted

--- test.js
+++
@@ -1,7 +0,0 @@
-// import {gameOver} from "./main.js";
-
-
-// it("should return the string, 'Game Over!'", () => {
-//   const output = gameOver(motherShip.currentPoints <= 0);
-//   expect(output).toBe("Game Over!");
-// });
